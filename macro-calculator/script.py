7f133b96e680bbc28141b2b9cc3b94443553848f
Oskar Legner <oskarlegner@gmail.com>
2023-09-03 17:24:51 +0200

    🚧 Add macroelements energy values

--- macro-calculator/script.py
+++ macro-calculator/script.py
@@ -1,10 +1,12 @@
-
+KCAL_PER_GRAM_PROTEIN = 4
+KCAL_PER_GRAM_CARBS = 4
+KCAL_PER_GRAM_FAT = 9
 
 protein = 0
 carbs = 0
 fat = 0
 
-def some_function():
+def count_energy_proportions():
     print("Hello World")
 
 
