63ebaf7af5ca0ae518a005ce37d18a738b0a4a92
Oskar Legner <oskarlegner@gmail.com>
2023-09-05 13:06:50 +0200

    📦️ Replace PyGithub with lowlevel requests package

--- git-fill-1-day-gaps.py
+++ git-fill-1-day-gaps.py
@@ -1,22 +1,12 @@
-# from git import Repo
-from github import Github
-from github import Auth
+import requests
 
+API_URL = "https://api.github.com"
 
 # Note: this is a one day token, so it will expire 06/10/2023
-auth = Auth.Token("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")
+# TODO: get token as program argument
+AUTH_TOKEN = "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"
 
-# First create a Github instance:
 
-# Public Web Github
-g = Github(auth=auth)
-
-# Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", auth=auth)
-
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
-
-# To close connections after use
-g.close()
+response = requests.get(API_URL + "/user", auth=("token", AUTH_TOKEN))
+if (200 == response.status_code):
+    print(response.json())
