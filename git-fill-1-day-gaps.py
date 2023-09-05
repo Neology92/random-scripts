df4e51f186ab99c9c7be3fdf79c3a4feda70e9ba
Oskar Legner <oskarlegner@gmail.com>
2023-09-05 13:36:10 +0200

    🔐 Revoke hardcoded GH token before pushing repo to the origin

--- git-fill-1-day-gaps.py
+++ git-fill-1-day-gaps.py
@@ -2,9 +2,8 @@ import requests
 
 API_URL = "https://api.github.com"
 
-# Note: this is a one day token, so it will expire 06/10/2023
-# TODO: get token as program argument
-AUTH_TOKEN = "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"
+# TODO: get new token
+AUTH_TOKEN = "ghp_..."
 
 
 response = requests.get(API_URL + "/user", auth=("token", AUTH_TOKEN))
