7477821c4217cd238c741988617b9c8a6c15ee7d
Oskar Legner <oskarlegner@gmail.com>
2023-09-05 13:34:31 +0200

    💡 Add some instructions for the future

--- git-fill-1-day-gaps.py
+++ git-fill-1-day-gaps.py
@@ -10,3 +10,14 @@ AUTH_TOKEN = "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"
 response = requests.get(API_URL + "/user", auth=("token", AUTH_TOKEN))
 if (200 == response.status_code):
     print(response.json())
+
+
+def get_contributions():
+    response = requests.get(API_URL + "/user", auth=("token", AUTH_TOKEN))
+    if (200 == response.status_code):
+        print(response.json())
+
+
+# REST API might not have /contributions endpoint
+# Although the GraphQL API allows to get such data
+# https://docs.github.com/en/graphql/overview/explorer
