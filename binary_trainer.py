fbe3c98425147d2fb8d54b5f168a1222c9444c0c
Oskar Legner <oskarlegner@gmail.com>
2023-09-18 21:22:24 +0200

    ✨ Add binary trainer script

---
+++ binary_trainer.py
@@ -0,0 +1,26 @@
+from random import randrange
+
+
+def play():
+    print("=============================")
+    print("|      Binary trainer       |")
+    print("=============================\n")
+    print("Input the decimal number corresponding to the binary number shown.\n")
+    print("Press Ctrl+C to quit.\n\n")
+    game_loop()
+
+
+def game_loop():
+    while True:
+        num = randrange(0, 15)
+        reply = input("Input {:0>4} decimally: ".format(bin(num)[2:]))
+        if reply == str(num):
+            print("Correct!")
+        else:
+            print("Wrong!")
+
+        print("\n")
+
+
+if __name__ == "__main__":
+    play()
