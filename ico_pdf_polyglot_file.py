17cad131dde07041793aa26d544f06bb29d13e6a
Oskar Legner <oskarlegner@gmail.com>
2023-09-22 17:00:57 +0200

    💡 Add some directions

--- ico_pdf_polyglot_file.py
+++ ico_pdf_polyglot_file.py
@@ -14,6 +14,14 @@ ico_file = open("./test_data/favicon.ico", "rb")
 #   0	    2	    Reserved. Must always be 0.
 #   2	    2	    Specifies image type: 1 for icon (.ICO) image, 2 for cursor (.CUR) image. Other values are invalid.
 #   4	    2	    Specifies number of images in the file.
+
 icondir_bytes = ico_file.read(6)
 
 img_count = struct.unpack("<HHH", icondir_bytes)[2]
+
+
+# Next 16 bytes are the ICONDIRENTRY structure:
+# (source: https://en.wikipedia.org/wiki/ICO_(file_format))
+
+
+# PDF things: https://web.archive.org/web/20141010035745/http://gnupdf.org/Introduction_to_PDF
