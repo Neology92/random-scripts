42383a971bc3e4fbffbb45dce5f095a172f2c345
Oskar Legner <oskarlegner@gmail.com>
2023-09-22 16:36:33 +0200

    🎉 Begin ico-pdf polyglot file merger

---
+++ ico_pdf_polyglot_file.py
@@ -0,0 +1,19 @@
+# Inspired with https://github.com/tickelton/ico-pdf repo
+# and related PagedOut article: https://pagedout.institute/download/PagedOut_001_beta1.pdf (page 14)
+
+import struct
+
+
+ico_file = open("./test_data/favicon.ico", "rb")
+
+# First 6 bytes of the file are the ICONDIR structure:
+# (source: https://en.wikipedia.org/wiki/ICO_(file_format))
+#
+#   Offset  Size    Description
+#   ------  ----    -----------
+#   0	    2	    Reserved. Must always be 0.
+#   2	    2	    Specifies image type: 1 for icon (.ICO) image, 2 for cursor (.CUR) image. Other values are invalid.
+#   4	    2	    Specifies number of images in the file.
+icondir_bytes = ico_file.read(6)
+
+img_count = struct.unpack("<HHH", icondir_bytes)[2]
